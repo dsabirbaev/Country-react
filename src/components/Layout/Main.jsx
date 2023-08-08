90d9a9b7367065c76922d620517f9dfe8e720c13
Davranbek <davranbek_1995@mail.ru>
2023-08-08 23:47:15 +0500

    added loader and err message

--- src/components/Layout/Main.jsx
+++ src/components/Layout/Main.jsx
@@ -4,31 +4,29 @@ import Card from "../UI/Card/Card";
 import { useState, useEffect } from "react";
 import axios from "axios";
 const baseURL = "https://restcountries.com/v2/all";
+import Loader from "./Loader";
 
 const Main = () => {
 
-    const [country, setCountry] = useState([])
-
+    const [country, setCountry] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [errorMsg, setErrorMsg] = useState(false)
     useEffect(() => {
         async function fetchCountries(){
+            setLoading(true);
             try {
               const response = await axios.get(baseURL);
               setCountry(response.data);
-            } catch (error) {
-              console.log(error.message);
-            } finally {
-              console.log("loading");
-            }
+              setLoading(false)
+            } catch (err) {
+              setErrorMsg(err.message);
+              setLoading(false);
+            } 
         };
         fetchCountries();
-        
+
     }, [])
-
     
-      
-   
-        
-
     return (
         <section className="pt-[48px]">
             <div className="container mx-auto px-5">
@@ -45,11 +43,23 @@ const Main = () => {
 
                 <div className="grid grid-cols-4 gap-y-[67px]">
                     {
+                        loading ? <Loader/> : null
+                    }
+                    {   country.length > 0 ?
                         country.map((item, index) => {
                         
                             return <Card key={index} state={item}/>
-                        })
+                        }) : null
                     }
+
+                    {
+                        errorMsg ? (
+                            <div>
+                                <h3>{errorMsg}</h3>
+                            </div>)
+                        :null
+                    }
+
                     
                 </div>
             </div>
