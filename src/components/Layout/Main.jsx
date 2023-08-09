0cf677c6188c4fedfa37aae47ef732126c13b7db
Davranbek <davranbek_1995@mail.ru>
2023-08-09 21:12:46 +0500

    added search and category

--- src/components/Layout/Main.jsx
+++ src/components/Layout/Main.jsx
@@ -3,42 +3,92 @@ import search from "../../assets/icons/search.svg";
 import Card from "../UI/Card/Card";
 import { useState, useEffect } from "react";
 import axios from "axios";
-const baseURL = "https://restcountries.com/v2/all";
+
+import regions from "../db/regions";
+
+const baseURL = "https://restcountries.com/v2";
+
 import Loader from "./Loader";
 
 const Main = () => {
-
+    
+ 
     const [country, setCountry] = useState([]);
     const [loading, setLoading] = useState(false);
     const [errorMsg, setErrorMsg] = useState(false)
-    useEffect(() => {
-        async function fetchCountries(){
-            setLoading(true);
-            try {
-              const response = await axios.get(baseURL);
-              setCountry(response.data);
-              setLoading(false)
-            } catch (err) {
-              setErrorMsg(err.message);
-              setLoading(false);
-            } 
-        };
-        fetchCountries();
+    const [searchText, setSearchText] = useState('');
+  
+    async function fetchCountries(){
+        setLoading(true);
+        try {
+          const response = await axios.get(`${baseURL}/all`);
+          setCountry(response.data);
+          setLoading(false)
+        } catch (err) {
+          setErrorMsg(err.message);
+          setLoading(false);
+        } 
+    };
 
+    useEffect(() => {
+        fetchCountries();
     }, [])
+
+    async function searchCountry() {
+        try {
+          const response = await axios.get(`${baseURL}/name/${searchText}`);
+          setCountry(response.data);
+          if(!response.data.length){
+            fetchCountries()
+          }
+        } catch (err) {
+          setErrorMsg(err.message);
+        }
+    }
+
+    async function filterByRegion(region) {
+        try{
+            const response = await axios.get(`${baseURL}/region/${region}`);
+            setCountry(response.data);
+        }catch (err){
+            setErrorMsg(err.message);
+        }
+    }
+
+    function handleSearchCountry(e){
+        e.preventDefault();
+        searchCountry()
+    }
+
+    function handleFilterByRegion(e) {
+        e.preventDefault();
+        filterByRegion();
+    }
+    
+ 
+
     
     return (
         <section className="pt-[48px]">
             <div className="container mx-auto px-5">
                 <div className="flex items-center justify-between mb-[48px]">
-                    <div className="w-[480px] bg-white py-[19px] px-9 rounded-md flex gap-x-6 shadow-md">
+                    <form onSubmit={handleSearchCountry} autoComplete="off" className="w-[480px] bg-white py-[19px] px-9 rounded-md flex gap-x-6 shadow-md">
                         <img src={search} alt="search" />
-                        <input className="outline-none w-full text-[14px] text-[#848484]" type="text" placeholder="Search for a country…" />
-                    </div>
-                    <select className="oultine-none w-[200px] shadow-md py-[18px] px-6">
-                        <option className="oultine-none" disabled>Filter by Region</option>
-                        <option >Africa</option>
-                    </select>
+                        <input value={searchText} onChange={(e) => setSearchText(e.target.value)} className="outline-none w-full text-[14px] text-[#848484]" type="text" placeholder="Search for a country…" />
+                    </form>
+
+                    <form onSubmit={handleFilterByRegion}>
+                        <select value={regions.name} onChange={(e) => filterByRegion(e.target.value)} className="oultine-none w-[200px] shadow py-[18px] px-6">
+                            {
+                                regions.map((region, index) => (
+                                    <option key={index} value={region.name}>
+                                        {region.name}
+                                    </option>
+                                ))
+                            }
+                        </select>
+                    </form>
+                   
                 </div>
 
                 <div className="grid grid-cols-4 gap-y-[67px]">
---
+++ src/components/db/regions.jsx
@@ -0,0 +1,24 @@
+
+
+const regions = [
+    {
+        name: "Europe",
+    },
+    {
+        name: "Asia",
+    },
+    {
+        name: "Africa",
+    },
+    {
+        name: "Oceania",
+    },
+    {
+        name: "Americas",
+    },
+    {
+        name: "Antarctic",
+    },
+];
+
+export default regions;
